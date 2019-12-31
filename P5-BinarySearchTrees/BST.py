67d97d96b2ecd55d8022ebcb55ffd4c7118b3dec
singhvis29 <singhvis@iu.edu>
2019-12-31 10:15:56 -0500

    adding p3, p5 report and codes

--- P5-BinarySearchTrees/BST.py
+++ P5-BinarySearchTrees/BST.py
@@ -1,6 +1,10 @@
 
 class BinarySearchTree:
     def __init__(self, val):
+        """
+        Class for a Binary Search Tree Node object
+        :param val:
+        """
         self.parent = None
         self.val = val
         self.left = None
@@ -13,25 +17,35 @@ class BinarySearchTree:
 class BstFunctions:
     def __init__(self):
         self.tree_size = 0
+        self.sum_elements = 0
 
-    def tree_insert(self, tree, z):
-        y = None
-        x = tree
-        while x is not None:
-            y = x
-            if z > y.val:
-                x = x.right
-            else:
-                x = x.left
-        # z.p = y
-        if y is None:
-            tree.val = z
-        if y.val > z:
-            y.left = BinarySearchTree(z)
+    def insert(self, tree, z):
+        """
+        Function to insert an element in the given Binary Search Tree
+        :param tree: BST tree
+        :param z: element that has to be inserted
+        :return: BST with the element inserted
+        """
+        if tree is None:
+            return BinarySearchTree(z)
         else:
-            y.right = BinarySearchTree(z)
+            if z <= tree.val:
+                temp = self.insert(tree.left, z)
+                tree.left = temp
+                temp.parent = tree
+            else:
+                temp = self.insert(tree.right, z)
+                tree.right = temp
+                temp.parent = tree
+        return tree
 
     def contains(self, tree, k):
+        """
+        Function to search for an element in a Binary Search Tree
+        :param tree:
+        :param k:
+        :return:
+        """
         if tree is None:
             return tree
         elif k == tree.val:
@@ -42,18 +56,33 @@ class BstFunctions:
             return self.contains(tree.right, k)
 
     def inorder(self, tree):
+        """
+        Function to print the Binary Search Tree in increasing order
+        :param tree:
+        :return:
+        """
         if tree is not None:
             self.inorder(tree.left)
             print(tree.val)
             self.inorder(tree.right)
 
     def size(self, tree):
+        """
+        Function to determine the size of Binary Search Tree
+        :param tree:
+        :return:
+        """
         if tree is not None:
             self.tree_size += 1
             self.size(tree.left)
             self.size(tree.right)
 
     def smallest(self, tree):
+        """
+        Function to determine the smallest value in a Binary Search Tree
+        :param tree:
+        :return:
+        """
         x = tree
         while x is not None:
             smallest = x.val
@@ -61,49 +90,109 @@ class BstFunctions:
         return smallest
 
     def largest(self, tree):
+        """
+        Function to determine the largest value in a Binary Search Tree
+        :param tree:
+        :return:
+        """
         x = tree
         while x is not None:
             largest = x.val
             x = x.right
         return largest
 
-    def successor(self, tree, key):
-        x = tree
-        x.parent = None
-
-
-
-
+    def successor(self, key):
+        """
+        Function to determine the successor of a key in a Binary Search Tree
+        :param key:
+        :return:
+        """
         if key.right is not None:
             return self.smallest(key.right)
-        y = None
-        while y is not None and x == y.right:
-            x = y
-            y = y.parent
-        return y
+        p = key.parent
+        while p is not None:
+            if key != p.right:
+                break
+            key = p
+            p = p.parent
+        return p
+
+    def predecessor(self, key):
+        """
+        Function to determine the predecessor of a key in a Binary Search Tree
+        :param key:
+        :return:
+        """
+        if key.left is not None:
+            return self.largest(key.left)
+        p = key.parent
+        while p is not None:
+            if key != p.left:
+                break
+            key = p
+            p = p.parent
+        return p
+
+    def greater_sum_tree(self, tree):
+        """
+        Function to build a greater sum tree, initiates a DFS on the BST
+        :param tree:
+        :return:
+        """
+        self.dfs(tree)
+        return tree
+
+    def dfs(self, root):
+        """
+        Function to perform DFS starting from right most element and keep updating the sum of values visited and replace
+        the node with the sum
+        :param root:
+        :return:
+        """
+        if root is None:
+            return
+        self.dfs(root.right)
+        self.sum_elements += root.val
+        root.val = self.sum_elements - root.val
+        self.dfs(root.left)
+        return
 
 
-bst = BinarySearchTree(5)
-bst.left = BinarySearchTree(3)
-bst.right = BinarySearchTree(7)
-bst.left.left = BinarySearchTree(2)
-bst.left.right = BinarySearchTree(4)
-bst.right.left = BinarySearchTree(6)
-bst.right.right = BinarySearchTree(8)
-print(bst)
-
+# Binary Tree Initialization
 bst_functions = BstFunctions()
-bst_functions.tree_insert(bst, 9)
-bst_functions.tree_insert(bst, 1)
+bst = None
+
+bst = bst_functions.insert(bst, 5)
+bst = bst_functions.insert(bst, 3)
+bst = bst_functions.insert(bst, 7)
+bst = bst_functions.insert(bst, 2)
+bst = bst_functions.insert(bst, 4)
+bst = bst_functions.insert(bst, 6)
+bst = bst_functions.insert(bst, 8)
+bst = bst_functions.insert(bst, 9)
+bst = bst_functions.insert(bst, 1)
 print(bst)
+temp = bst.left.right.right
 
-print("Search", bst_functions.contains(bst, 7))
-print("Search", bst_functions.contains(bst, 13))
-
+# Binary Tree Function
+print("Searching for number 7:", bst_functions.contains(bst, 7))
+print("Searching for number 13: ", bst_functions.contains(bst, 13))
+print('Inorder print of Binary Search Tree')
 bst_functions.inorder(bst)
 
 bst_functions.size(bst)
 print('Tree Size:', bst_functions.tree_size)
 print("Smallest:", bst_functions.smallest(bst))
 print("Largest:", bst_functions.largest(bst))
-print("Successor:", bst_functions.successor(bst, bst.left.right))
+print('Element to find successor', bst_functions.contains(bst, 7).val)
+print("Successor:", bst_functions.successor(bst_functions.contains(bst, 7)))
+
+print('Element to find predecessor', bst_functions.contains(bst, 1).val)
+print("Predecessor:", bst_functions.predecessor(bst_functions.contains(bst, 1)))
+
+# print('Greater Sum Tree:', bst_functions.greater_sum_tree(bst))
+
+print("Inorder before GST:", bst_functions.inorder(bst))
+bst_functions.greater_sum_tree(bst)
+print("Inorder after GST:", bst_functions.inorder(bst))
+
